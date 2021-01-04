c32cc21be556800f973d4d06035fb21a555da327
cxl971453503 <971453503@qq.com>
2021-01-04 16:25:59 +0800

    master

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -6,19 +6,19 @@
                 <img src="../assets/logo.png" alt="">
             </div>
             <!-- 登陆表单区域 -->
-            <el-form :model="loginform" label-width="0px" class="login_form">
+            <el-form ref="loginFormRef" :model="loginform" :rules="loginFormRules" label-width="0px" class="login_form">
                 <!-- 用户名 -->
-                <el-form-item>
-                    <el-input v-model="loginform.username" prefix-icon="iconfont icon-user"></el-input>
+                <el-form-item prop="username">
+                    <el-input v-model="loginform.username" prefix-icon="iconfont icon-user" placeholder="用户名"></el-input>
                 </el-form-item>
                 <!-- 密码 -->
-                <el-form-item>
-                    <el-input type="password" v-model="loginform.password" prefix-icon="iconfont icon-3702mima"></el-input>
+                <el-form-item prop="password">
+                    <el-input type="password" v-model="loginform.password" prefix-icon="iconfont icon-3702mima" placeholder="密码"></el-input>
                 </el-form-item>
                 <!-- 按钮 -->
                 <el-form-item class="btns">
-                    <el-button type="primary">登陆</el-button>
-                    <el-button type="info">重置</el-button>
+                    <el-button type="primary" @click="login">登陆</el-button>
+                    <el-button type="info" @click="resetLoginForm">重置</el-button>
                 </el-form-item>
             </el-form>
         </div>
@@ -31,9 +31,43 @@ export default {
         return{
             // 这是登陆表单的数据绑定对象
             loginform:{
-                username: '',
-                password: ''
+                username: 'admin',
+                password: '123456'
+            },
+            // 这是表单的验证规则对象
+            loginFormRules:{
+                // 验证用户名是否合法
+                username:[
+                    {required: true, message: "请输入登陆名称", trigger: "blur"},
+                    {min: 3, max: 10, message: "长度在 3 到 10 个字符之间", trigger: "blur"}
+                ],
+                // 验证密码是否合法
+                password:[
+                    {required: true, message: "请输入登陆密码", trigger: "blur"},
+                    {min: 6, max: 15, message: "长度在 6 到 15 个字符之间", trigger: "blur"}
+                ]
             }
+        }
+    },
+    methods: {
+        // 点击重置按钮，重置登陆表单
+        resetLoginForm() {
+            // console.log(this);
+            this.$refs.loginFormRef.resetFields();
+        },
+        login() {
+            this.$refs.loginFormRef.validate(async valid => {
+                if(!valid) return;
+                const {data:res} = await this.$http.post('login', this.loginform);
+                if(res.meta.status !== 200) return this.$message.error(res.meta.msg);
+                this.$message.success('登陆成功');
+                // 1. 将登陆成功之后的 token 保存到客户端的 sessionStorage 中
+                //    1.1 项目中除了登陆之外的其他API接口，必须在登陆之后才能访问
+                //    1.2 token 只应在当前网站打开期间生效，所以将 token 保存在 sessionStorage 中
+                window.sessionStorage.setItem('token', res.data.token);
+                // 2. 通过编程式导航跳转到后台主页，路由是 /home
+                this.$router.push('/home');
+            });
         }
     }
 }
--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Login from './components/Login.vue'
+import Home from './components/Home.vue'
 
 Vue.use(VueRouter)
 
@@ -13,8 +14,26 @@ const router = new VueRouter({
     {
       path: '/login',
       component: Login
+    },
+    {
+      path: '/home',
+      component: Home
     }
   ]
+})
+
+// 挂在路由导航守卫
+router.beforeEach((to, from, next) => {
+  // to 将要访问的路径
+  // from 代表从哪个路径来 
+  // next 是一个行数，表示方形
+  // next() 放行 next('/login') 强制跳转
+  if(to.path === '/login') return next();
+  // 获取token
+  const tokenStr = window.sessionStorage.getItem('token');
+  // 没有token，强制跳转到登录页
+  if(!tokenStr) return next('/login');
+  next();
 })
 
 export default router
