53c2bb62787628a152de6f39799ee32bd13b5cc0
cxl971453503 <971453503@qq.com>
2021-01-06 20:08:34 +0800

    master

---
+++ src/components/power/Rights.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <h3>
+            权限管理组件
+        </h3>
+    </div>
+</template>
+
+<script>
+export default {
+    data () {
+        return {
+            
+        }
+    }
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
